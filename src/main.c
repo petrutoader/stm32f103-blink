90cdbb8f20d99cc4f97ba96bc1d806751ebb777d
Petru-Sebastian Toader <petru@phyramid.com>
2019-12-25 17:33:58 +0200

    fixed indentation

--- src/main.c
+++ src/main.c
@@ -22,31 +22,19 @@ void delay(uint32_t duration);
 static volatile uint32_t _millis = 0;
 
 int _write(int file, const char *ptr, ssize_t len) {
-    // If the target file isn't stdout/stderr, then return an error
-    // since we don't _actually_ support file handles
-    if (file != STDOUT_FILENO && file != STDERR_FILENO) {
-        // Set the errno code (requires errno.h)
-        errno = EIO;
-        return -1;
+  if (file != STDOUT_FILENO && file != STDERR_FILENO) {
+    errno = EIO;
+    return -1;
+  }
+
+  int i;
+  for (i = 0; i < len; i++) {
+    if (ptr[i] == '\n') {
+      usart_send_blocking(USART2, '\r');
     }
-
-    // Keep i defined outside the loop so we can return it
-    int i;
-    for (i = 0; i < len; i++) {
-        // If we get a newline character, also be sure to send the carriage
-        // return character first, otherwise the serial console may not
-        // actually return to the left.
-        if (ptr[i] == '\n') {
-            usart_send_blocking(USART2, '\r');
-        }
-
-        // Write the character to send to the USART1 transmit buffer, and block
-        // until it has been sent.
-        usart_send_blocking(USART2, ptr[i]);
-    }
-
-    // Return the number of bytes we sent
-    return i;
+    usart_send_blocking(USART2, ptr[i]);
+  }
+  return i;
 }
 
 
@@ -73,7 +61,6 @@ uint32_t millis(void) {
   return _millis;
 }
 
-// Delay a given number of milliseconds in a blocking manner
 void delay(uint32_t duration) {
   const uint32_t until = millis() + duration;
   while (millis() < until);
@@ -89,7 +76,6 @@ static void usart_setup(void) {
 	usart_set_parity(USART2, USART_PARITY_NONE);
 	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
 
-	/* Finally enable the USART. */
 	usart_enable(USART2);
 }
 
